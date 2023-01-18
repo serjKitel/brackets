6eb5f8f0878ce934414e4310ed630ac0963c3d9f
serjKitel <kitselsiarheivladimirovich@gmail.com>
2023-01-18 20:15:50 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let arr = [];
+  let copyStr = str;
+  let strLength = str.length;
+
+  if (str.length % 2 !== 0) {
+    return false;
+  } else {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      arr[i] = bracketsConfig[i][0] + bracketsConfig[i][1];
+    }
+    for (let i = 0; i < copyStr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        str = str.split(arr[j]).join('');
+        strLength = str.length;
+      }
+    }
+    if (strLength === 0) {
+      return true;
+    }
+  }
+  return false;
 }
